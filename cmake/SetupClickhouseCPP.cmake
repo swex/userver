2dd416503139657050b810c895773912d27913b7
Alexey Mednyy <a.mednyy@ctdl.ru>
2026-06-29 16:07:14 +0300

    update clickhouse-cpp to 2.6.2

--- cmake/SetupClickhouseCPP.cmake
+++ cmake/SetupClickhouseCPP.cmake
@@ -26,8 +26,8 @@ include(SetupAbseil)
 
 cpmaddpackage(
     NAME clickhouse-cpp
-    VERSION 2.6.0
-    GIT_TAG 69195246a3b39542c397ef27df9f46ec4a4bf206
+    VERSION 2.6.2
+    GIT_TAG 001025cd72d904104c15657e85e0fb6b0ec58e14
     GITHUB_REPOSITORY ClickHouse/clickhouse-cpp
     SYSTEM
     OPTIONS "WITH_SYSTEM_ABSEIL ON" "WITH_SYSTEM_LZ4 ON" "DEBUG_DEPENDENCIES OFF"
